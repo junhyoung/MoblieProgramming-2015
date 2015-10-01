fbdc6ca4e864b1a2f0ab33f24a5ffa2d4645be64
준형 <@cjh>
2015-10-01 19:05:50 +0900

    Lab5 예제 1,2,3

---
+++ src/kr/ac/kookmin/embedded/overriding/Dragon.java
@@ -0,0 +1,9 @@
+package kr.ac.kookmin.embedded.overriding;
+
+public class Dragon extends Monster {
+	boolean frighten(int degree){
+		System.out.println("breath fire");
+		return true;
+	}
+
+}
---
+++ src/kr/ac/kookmin/embedded/overriding/Monster.java
@@ -0,0 +1,8 @@
+package kr.ac.kookmin.embedded.overriding;
+
+public class Monster {
+	boolean frighten(int d){
+		System.out.println("arrrgh");
+		return true;
+	}
+}
